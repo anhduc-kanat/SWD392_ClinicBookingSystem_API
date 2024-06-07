9a8a56ebb25213d32df8aaffd60eb32f04662694
ducnase171103@fpt.edu.vn <naduccva@gmail.com>
2024-06-07 15:35:39 +0700

    role

---
+++ ClinicBookingSystem/Controllers/RoleController.cs
@@ -0,0 +1,16 @@
+﻿using ClinicBookingSystem_Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicBookingSystem_API.Controllers;
+
+[ApiController]
+[Route("/api/role")]
+public class RoleController : ControllerBase
+{
+    private readonly IRoleService _roleService;
+    public RoleController(IRoleService roleService)
+    {
+        _roleService = roleService;
+    }
+    
+}
--- ClinicBookingSystem_Service/Service/RoleService.cs
+++ ClinicBookingSystem_Service/Service/RoleService.cs
@@ -6,19 +6,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using ClinicBookingSystem_Service.Models.BaseResponse;
+using ClinicBookingSystem_Service.Models.Enums;
+using ClinicBookingSystem_Service.Models.Request.Role;
+using ClinicBookingSystem_Service.Models.Response.Role;
 
 namespace ClinicBookingSystem_Service.Service
 {
     public class RoleService : IRoleService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
         public RoleService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
-        public Task<Role> GetRoleByName(string roleName)
+        public async Task<BaseResponse<GetRoleResponse>> GetRoleByName(string roleName)
         {
-            return _unitOfWork.RoleRepository.GetRoleByName(roleName);
+            GetRoleResponse response = _mapper.Map<GetRoleResponse>(await _unitOfWork.RoleRepository.GetRoleByName(roleName));
+            return new BaseResponse<GetRoleResponse>("Successful", StatusCodeEnum.OK_200, response);
+        }
+        private async Task<BaseResponse<CreateRoleResponse>> CreateRole(CreateRoleRequest request)
+        {
+            try
+            {
+                Role role = _mapper.Map<Role>(request);
+                var createdRole = await _unitOfWork.RoleRepository.AddAsync(role);
+                await _unitOfWork.SaveChangesAsync();
+                return new BaseResponse<CreateRoleResponse>("Create Role Successfully!", StatusCodeEnum.Created_201);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<CreateRoleResponse>("Error at CreateRole Service: " + ex.Message, StatusCodeEnum.InternalServerError_500);
+            }
         }
     }
 }
