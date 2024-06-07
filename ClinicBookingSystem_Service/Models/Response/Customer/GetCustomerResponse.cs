aaef9b10f4eb52ac4237ea74efd28e4d4ca5a7bb
qunngh <hoquang1031@gmail.com>
2024-06-07 14:28:26 +0700

    fix return value in customer

--- ClinicBookingSystem_Service/Models/Response/Customer/GetCustomerResponse.cs
+++ ClinicBookingSystem_Service/Models/Response/Customer/GetCustomerResponse.cs
@@ -10,8 +10,6 @@ namespace ClinicBookingSystem_Service.Models.Response.Customer
     {
         public string? PhoneNumber { get; set; }
 
-        public string? Password { get; set; }
-
         public string? FirstName { get; set; }
 
         public string? LastName { get; set; }
