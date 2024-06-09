a0e7b08713a3f2974fc6a025503ea526c8629bcd
ducnase171103@fpt.edu.vn <naduccva@gmail.com>
2024-06-09 14:23:53 +0700

    create appointment

--- ClinicBookingSystem_Service/Models/Request/Appointment/CreateAppointmentRequest.cs
+++ ClinicBookingSystem_Service/Models/Request/Appointment/CreateAppointmentRequest.cs
@@ -2,5 +2,11 @@
 
 public class CreateAppointmentRequest
 {
-    
+    public DateTime Date { get; set; }
+    public bool? IsPeriod { get; set; } = false;
+    public int? ReexamUnit { get; set; }
+    public int? ReexamNumber { get; set; }
+    public bool? IsTreatment { get; set; } = false;
+    public int ServiceId { get; set; }
+    public int? SlotId { get; set; }
 }
