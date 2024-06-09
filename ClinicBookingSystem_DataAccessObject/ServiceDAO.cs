1d5ffd476a34af39452b3f01d8c8d2d4c3623dfa
Nguyen Hoang My Phuoc <nguyenhoangmyphuoc2511@gmail.com>
2024-06-09 13:41:50 +0700

    Create ServiceDAO.cs

---
+++ ClinicBookingSystem_DataAccessObject/ServiceDAO.cs
@@ -0,0 +1,58 @@
+﻿using ClinicBookingSystem_BusinessObject.Entities;
+using ClinicBookingSystem_DataAccessObject.BaseDAO;
+using ClinicBookingSystem_DataAcessObject.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicBookingSystem_DataAccessObject
+{
+    public class ServiceDAO : BaseDAO<Service>
+    {
+        private readonly ClinicBookingSystemContext _context;
+
+        public ServiceDAO(ClinicBookingSystemContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Service>> GetAllSlots()
+        {
+            return await _context.Services.ToListAsync();
+        }
+        //
+        public async Task<Service> GetServiceById(int id)
+        {
+            var service = await _context.Services.FindAsync(id);
+
+            return service;
+        }
+        //
+        public async Task<Service> CreateService(Service service)
+        {
+            _context.Services.Add(service);
+            await _context.SaveChangesAsync();
+
+            return service;
+        }
+
+        public async Task<Service> UpdateService(Service service)
+        {
+            var existingService = await GetServiceById(service.Id);
+            _context.Services.Update(service);
+            await _context.SaveChangesAsync();
+            return existingService;
+        }
+        //
+        public async Task<Service> DeleteService(int id)
+        {
+            var existingService = await GetServiceById(id);
+            _context.Services.Remove(existingService);
+            await _context.SaveChangesAsync();
+            return existingService;
+        }
+    }
+}
