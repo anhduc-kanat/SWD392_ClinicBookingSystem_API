6dcc34c8d8642063dd36e9774dcb83a87ba56ac8
ducnase171103@fpt.edu.vn <naduccva@gmail.com>
2024-06-11 13:29:18 +0700

    mapping role

---
+++ ClinicBookingSystem_Service/Mapping/MappingRole.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ClinicBookingSystem_BusinessObject.Entities;
+using ClinicBookingSystem_Service.Models.Request.Role;
+using ClinicBookingSystem_Service.Models.Response.Role;
+
+namespace ClinicBookingSystem_Service.Mapping;
+
+public class MappingRole : Profile
+{
+    public MappingRole()
+    {
+        
+        CreateMap<CreateRoleRequest, Role>();
+        CreateMap<Role, CreateRoleResponse>().ReverseMap();
+        CreateMap<Role, GetRoleResponse>().ReverseMap();
+        CreateMap<Role, DeleteRoleResponse>().ReverseMap();
+        CreateMap<Role, UpdateRoleResponse>().ReverseMap();
+        CreateMap<UpdateRoleRequest, Role>();
+    }
+}
