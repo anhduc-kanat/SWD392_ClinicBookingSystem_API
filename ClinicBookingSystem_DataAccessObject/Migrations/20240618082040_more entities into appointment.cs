39aef9d9ec11008d4a8bbd3379b23d3097129111
ducnase171103@fpt.edu.vn <naduccva@gmail.com>
2024-06-18 16:39:47 +0700

    cai nay moi fix du thu ne

---
+++ ClinicBookingSystem_DataAccessObject/Migrations/20240618082040_more entities into appointment.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicBookingSystem_DataAccessObject.Migrations
+{
+    /// <inheritdoc />
+    public partial class moreentitiesintoappointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserBookingName",
+                table: "Appointments",
+                newName: "UserAccountName");
+
+            migrationBuilder.RenameColumn(
+                name: "UserBookingId",
+                table: "Appointments",
+                newName: "UserAccountId");
+
+            migrationBuilder.RenameColumn(
+                name: "StaffBookingName",
+                table: "Appointments",
+                newName: "StaffAccountName");
+
+            migrationBuilder.RenameColumn(
+                name: "StaffBookingId",
+                table: "Appointments",
+                newName: "StaffAccountId");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Appointments",
+                newName: "DentistTreatmentName");
+
+            migrationBuilder.RenameColumn(
+                name: "DentistBookingName",
+                table: "Appointments",
+                newName: "DentistAccountName");
+
+            migrationBuilder.RenameColumn(
+                name: "DentistBookingId",
+                table: "Appointments",
+                newName: "DentistTreatmentId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DentistAccountId",
+                table: "Appointments",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DentistAccountId",
+                table: "Appointments");
+
+            migrationBuilder.RenameColumn(
+                name: "UserAccountName",
+                table: "Appointments",
+                newName: "UserBookingName");
+
+            migrationBuilder.RenameColumn(
+                name: "UserAccountId",
+                table: "Appointments",
+                newName: "UserBookingId");
+
+            migrationBuilder.RenameColumn(
+                name: "StaffAccountName",
+                table: "Appointments",
+                newName: "StaffBookingName");
+
+            migrationBuilder.RenameColumn(
+                name: "StaffAccountId",
+                table: "Appointments",
+                newName: "StaffBookingId");
+
+            migrationBuilder.RenameColumn(
+                name: "DentistTreatmentName",
+                table: "Appointments",
+                newName: "FullName");
+
+            migrationBuilder.RenameColumn(
+                name: "DentistTreatmentId",
+                table: "Appointments",
+                newName: "DentistBookingId");
+
+            migrationBuilder.RenameColumn(
+                name: "DentistAccountName",
+                table: "Appointments",
+                newName: "DentistBookingName");
+        }
+    }
+}
--- ClinicBookingSystem_Service/Models/Response/Appointment/GetAppointmentResponse.cs
+++ ClinicBookingSystem_Service/Models/Response/Appointment/GetAppointmentResponse.cs
@@ -12,14 +12,19 @@ public class GetAppointmentResponse
     public bool IsPeriod { get; set; }
     public int ReExamUnit { get; set; }
     public int ReExamNumber { get; set; }
-    public bool IsApproved { get; set; }
     public AppointmentStatus Status { get; set; }
     public string Description { get; set; }
     public string FeedBack { get; set; }
     public bool IsTreatment { get; set; }
-    public string DentistName { get; set; }
-    public string UserBookingName { get; set; }
+    public string DentistTreatmentName { get; set; }
+    public string UserAccountName { get; set; }
     public string PatientName { get; set; }
+    public string PatientGender { get; set; }
+    public string PatientPhoneNumber { get; set; }
+    public string PatientAddress { get; set; }
+    public string PatientDateOfBirth { get; set; }
+    public string PatientCCCD { get; set; }
+    public string PatientType { get; set; }
     public string ServiceName { get; set; }
     public ServiceType ServiceType { get; set; }
     public string SlotName { get; set; }
