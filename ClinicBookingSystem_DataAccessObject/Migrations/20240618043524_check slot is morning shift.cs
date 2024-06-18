fadbdf1715dbafeaec4514ef5fc73806fd2b3ab7
ducnase171103@fpt.edu.vn <naduccva@gmail.com>
2024-06-18 12:07:48 +0700

    add property into slot, staff checkin customer appointment

---
+++ ClinicBookingSystem_DataAccessObject/Migrations/20240618043524_check slot is morning shift.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicBookingSystem_DataAccessObject.Migrations
+{
+    /// <inheritdoc />
+    public partial class checkslotismorningshift : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMorningShift",
+                table: "Slots",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMorningShift",
+                table: "Slots");
+        }
+    }
+}
