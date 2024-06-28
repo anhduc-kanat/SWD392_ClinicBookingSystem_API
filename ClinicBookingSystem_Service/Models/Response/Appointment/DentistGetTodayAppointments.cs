7e8eaa42f31e7642a138924b807c2075ccb991b0
kanatsumi <ducnase171103@fpt.edu.vn>
2024-06-28 14:51:28 +0700

    dentist get appointment by day

---
+++ ClinicBookingSystem_Service/Models/Response/Appointment/DentistGetTodayAppointments.cs
@@ -0,0 +1,27 @@
+﻿using ClinicBookingSystem_BusinessObject.Enums;
+
+namespace ClinicBookingSystem_Service.Models.Response.Appointment;
+
+public class DentistGetTodayAppointments
+{
+public int? Id { get; set; }
+public DateOnly? Date { get; set; }
+public bool? IsReExam { get; set; }
+public AppointmentStatus? Status { get; set; }
+public string? Description { get; set; }
+public string? FeedBack { get; set; }
+public int? UserAccountId { get; set; }
+public string? UserAccountName { get; set; }
+public int? PatientId { get; set; }
+public string? PatientName { get; set; }
+public string? PatientGender { get; set; }
+public string? PatientPhoneNumber { get; set; }
+public string? PatientAddress { get; set; }
+public string? PatientDateOfBirth { get; set; }
+public string? PatientCCCD { get; set; }
+public string? PatientType { get; set; }
+public int? SlotId { get; set; }
+public string? SlotName { get; set; }
+public TimeSpan? StartAt { get; set; }
+public TimeSpan? EndAt { get; set; }
+}
