59e33aa0deadf304e212f6b358e301df12b75d72
unknown <kalen153246789@gmail.com>
2024-07-03 13:50:29 +0700

    get all exam services

--- ClinicBookingSystem_Repository/IRepositories/IServiceRepository.cs
+++ ClinicBookingSystem_Repository/IRepositories/IServiceRepository.cs
@@ -6,6 +6,7 @@ namespace ClinicBookingSystem_Repository.IRepositories;
 public interface IServiceRepository : IBaseRepository<BusinessService>
 {
     public Task<IEnumerable<BusinessService>> GetAllServices();
+    public Task<IEnumerable<BusinessService>> GetAllExamServices();
     public Task<BusinessService> GetServiceById(int id);
     public Task<BusinessService> CreateService(BusinessService businessService);
     public Task<BusinessService> UpdateService(BusinessService businessService);
