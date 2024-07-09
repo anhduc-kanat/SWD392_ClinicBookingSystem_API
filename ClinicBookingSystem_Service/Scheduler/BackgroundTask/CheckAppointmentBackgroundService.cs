18ea9230b2b6b91c87cfb7e2523c9faf6cd6a06d
kanatsumi <ducnase171103@fpt.edu.vn>
2024-07-10 02:45:38 +0700

    schedule job to check meeting status, if datetime.now equals to meetings date => update meeting status

--- ClinicBookingSystem_Service/Scheduler/BackgroundTask/CheckAppointmentBackgroundService.cs
+++ ClinicBookingSystem_Service/Scheduler/BackgroundTask/CheckAppointmentBackgroundService.cs
@@ -1,8 +1,10 @@
-﻿using ClinicBookingSystem_Repository.IRepositories;
+﻿using ClinicBookingSystem_BusinessObject.Enums;
+using ClinicBookingSystem_Repository.IRepositories;
 using ClinicBookingSystem_Service.RabbitMQ.IService;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ClinicBookingSystem_Service.Scheduler.BackgroundTask;
 
@@ -21,13 +23,38 @@ public class CheckAppointmentBackgroundService : BackgroundService
     }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var provider = _serviceProvider.CreateScope();
-        var unitOfWork = provider.ServiceProvider.GetRequiredService<IUnitOfWork>();
-        _logger.LogInformation("5 secs job: CheckAppointmentBackgroundService is starting.");
-        var appointments = await unitOfWork.AppointmentRepository.GetTodayMeetingTreatmentAppointment();
-        foreach (var appointment in appointments)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            
+            var provider = _serviceProvider.CreateScope();
+            var unitOfWork = provider.ServiceProvider.GetRequiredService<IUnitOfWork>();
+            _logger.LogInformation("5 secs job: CheckAppointmentBackgroundService is starting.");
+        
+        
+            /*var appointments = await unitOfWork.AppointmentRepository.GetTodayMeetingTreatmentAppointment();
+            if(appointments.IsNullOrEmpty()) return;
+            foreach (var appointment in appointments)
+            {
+
+            }*/
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
         }
+        
     }
+
+    /*private async Task CheckMeetingStatus(IUnitOfWork unitOfWork)
+    {
+        _logger.LogInformation("Check MeetingStatus is starting.");
+        _logger.LogInformation($"{DateTime.Now}");
+        var meetings = await unitOfWork.MeetingRepository.GetMeetingByToday(DateTime.Now);
+        if (meetings.IsNullOrEmpty()) return;
+        foreach (var meeting in meetings)
+        {
+            _logger.LogInformation($"{meeting.Id} - {meeting.Status}");
+            if (meeting.Status != MeetingStatus.Future) continue;
+            meeting.Status = MeetingStatus.Waiting;
+            await unitOfWork.MeetingRepository.UpdateAsync(meeting);
+        }
+        await unitOfWork.SaveChangesAsync();
+        _logger.LogInformation("Check MeetingStatus has been done.");
+    }*/
 }
