57f930aab314c47ceb550d4fb93626cc7f6ee746
qunngh <hoquang1031@gmail.com>
2024-07-10 15:17:21 +0700

    fix transaction fix service

---
+++ ClinicBookingSystem_Service/Models/Response/AppointmentService/GetAppointmentServiceName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicBookingSystem_Service.Models.Response.AppointmentService
+{
+    public class GetAppointmentServiceName
+    {
+        public string ServiceName {  get; set; }
+    }
+}
