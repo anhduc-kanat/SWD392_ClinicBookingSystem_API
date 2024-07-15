ae3222d9dc2d032faa034eb8043a10cb76cad82b
kanatsumi <ducnase171103@fpt.edu.vn>
2024-07-16 03:51:40 +0700

    config and send email when create new dentist

---
+++ ClinicBookingSystem/wwwroot/View/NotificationEmailTemplate.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClinicBookingSystem.wwwroot.View;
+
+public class NotificationEmailTemplate : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ ClinicBookingSystem_Service/Notification/EmailNotification/NotificationEmailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicBookingSystem_Service.Notification.EmailNotification;
+
+public class NotificationEmailModel : IEmailModel
+{
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+}
